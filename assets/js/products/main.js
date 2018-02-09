09908b7d0a109258817fabf997a8b56329909a1d
jaquepaschoal <jaque-paschoal@hotmail.com>
2018-02-09 21:32:42 -0200

    Front-End verificação checkbox página produtos

---
+++ assets/js/products/main.js
@@ -0,0 +1,44 @@
+
+
+
+// var theCheckboxes = $(".pricing-levels-3 input[type='checkbox']");
+// theCheckboxes.click(function()
+// {
+//     if (theCheckboxes.filter(":checked").length > 3)
+//         $(this).removeAttr("checked");
+// });
+
+(function(){
+
+  'use strict';
+
+  var check = document.querySelectorAll('[data-js="checkbox"]');
+  var arr = [];
+
+  check.forEach(function(atual){
+    atual.addEventListener('click',function(event){
+      event.preventDefault();
+
+      if(atual.className == 'teste'){
+        arr = arr.filter(function(elemento){
+          return elemento !== atual;
+        });
+        atual.classList.remove('teste');
+      }
+      else {
+        if(arr.length < 2){
+          arr.push(atual);
+          atual.classList.add('teste');
+        }
+        else {
+          var remove = arr.pop();
+          remove.classList.remove('teste'); 
+          atual.classList.add('teste');
+          arr.push(atual);
+        }
+      }
+    })
+  })
+
+
+})();
