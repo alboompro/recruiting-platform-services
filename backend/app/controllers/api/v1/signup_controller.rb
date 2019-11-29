0a0966de6d3f3a550d0c6b6dfaab99ca6342d9d3
AbelJesus3 <abel.jesus.silva@outlook.com>
2019-11-29 19:55:21 -0300

    Implementação endpoint "signup" (Backend)

--- backend/app/controllers/api/v1/signup_controller.rb
+++ backend/app/controllers/api/v1/signup_controller.rb
@@ -1,5 +1,24 @@
 class Api::V1::SignupController < ApplicationController
     # POST - /api/v1/signup
-    def index
+    def create
+        name = params[:name]
+        email = params[:email]
+
+        client_exists = Client.exists?(email: email)
+        if(client_exists)
+            render :nothing => true, :status => :unauthorized
+            return
+        end
+        
+        coupon_code = generate_coupon_code
+        Client.create(name: name, email: email, coupon_code: coupon_code, created_at: DateTime.now)
+        
+        render :nothing => true, :status => :ok
+    end
+
+    private
+
+    def generate_coupon_code
+        return (0...8).map { (65 + rand(26)).chr }.join
     end
 end
