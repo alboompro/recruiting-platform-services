02b7f410e789f89e7ce7b38d246114fe58a63761
abjesus <abel.jesus.silva@outlook.com>
2019-12-04 14:27:07 -0300

    Implementado roteamento das páginas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,17 @@ import Products from './components/products'
 import Complements from './components/complements'
 import Final from './components/final'
 import { Container } from './styles'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 export default () => (
   <Container>
-    <Final />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact={true} component={Signup}/>
+        <Route path="/products" component={Products}/>
+        <Route path="/complements" component={Complements}/>
+        <Route path="/final" component={Final}/>
+      </Switch>
+    </BrowserRouter>
   </Container>
 );
