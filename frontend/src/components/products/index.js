932fb613216b3b892443fb4da0853630c3d7d39c
abjesus <abel.jesus.silva@outlook.com>
2019-12-05 12:07:44 -0300

    Tela products parcialmente implementada

--- frontend/src/components/products/index.js
+++ frontend/src/components/products/index.js
@@ -1,52 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Image, ListViewItem, ContainerBody, ContainerButton, Checkbox, ContainerCheckBox } from './styles'
 import Header from '../header';
 import Divider from '../divider';
 import dualcoffee from '../../assets/images/cafe-duplo.svg';
+import { getRequest } from '../../utils/base-axios'
 
-export default () => (
-  <Container>
-    <ContainerBody>
-      <Header />
-      <Divider style={{height: "3px", "margin-bottom": "27px"}} />
-      <ListViewItem>
-        <Image src={dualcoffee} alt="logo" />
-        <div>
-          <span>Café Duplo</span>
-          <span>0:45 min</span>
-        </div>
-        <ContainerCheckBox>
-          <Checkbox checked={false} onChange={(e) => console.log(e)}/>
-        </ContainerCheckBox>
-      </ListViewItem>
-      <Divider style={{height: "1px", "margin-bottom": "20px"}} />
-      <ListViewItem>
-        <Image src={dualcoffee} alt="logo" />
-        <div>
-          <span>Café Duplo</span>
-          <span>0:45 min</span>
-        </div>
-      </ListViewItem>
-      <Divider style={{height: "1px", "margin-bottom": "20px"}} />
-      <ListViewItem>
-        <Image src={dualcoffee} alt="logo" />
-        <div>
-          <span>Café Duplo</span>
-          <span>0:45 min</span>
-        </div>
-      </ListViewItem>
-      <Divider style={{height: "1px", "margin-bottom": "20px"}} />
-      <ListViewItem>
-        <Image src={dualcoffee} alt="logo" />
-        <div>
-          <span>Café Duplo</span>
-          <span>0:45 min</span>
-        </div>
-      </ListViewItem>
-      <Divider style={{height: "1px", "margin-bottom": "20px"}} />
-    </ContainerBody>
-    <ContainerButton>
-      <button>Continuar</button>
-    </ContainerButton>
-  </Container>
-)
+class Products extends Component {
+
+  state = {
+    products: []
+  }
+
+  loadData(){
+    getRequest().get('/api/v1/product')
+      .then(res => {
+        this.setState({ products: res.data })
+      });
+  }
+
+  handleContinueButton = () => this.props.history.push('/complements')  
+
+  render(){
+    return (
+      <Container>
+        <ContainerBody>
+          <Header />
+          <Divider style={{height: "3px", "marginBottom": "27px"}} />
+          {this.state.products.map(product => 
+            <div>
+              <ListViewItem>            
+                <Image src={dualcoffee} alt="logo" />
+                <div>
+                  <span>{product.name}</span>
+                  <span>{product.time}</span>
+                </div>
+                <ContainerCheckBox>
+                  <Checkbox checked={false} onChange={(e) => console.log(e)}/>
+                </ContainerCheckBox>
+              </ListViewItem>
+              <Divider style={{height: "1px", "marginBottom": "20px"}} />
+            </div>
+          )}
+        </ContainerBody>
+        <ContainerButton>
+          <button onClick={this.handleContinueButton}>Continuar</button>
+        </ContainerButton>
+      </Container>
+    )
+  }
+}
+
+export default Products;
